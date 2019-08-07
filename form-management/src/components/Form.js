22dcff1889e22d2e22eedb342749e97639c1cff9
Cory Hedeen <hedeencory@gmail.com>
2019-08-07 16:05:18 -0400

    Build out form

---
+++ form-management/src/components/Form.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const UserForm = ({errors, touched}) => {
+  return(
+    <Form>
+      <Field type="text" name="name" placeholder="name"/>
+      <Field type="email" name="email" placeholder="email"/>
+      <Field type="password" name="password" placeholder="password"/>
+      <label>
+          Terms Of Use
+          <Field
+            type="checkbox"
+            name="termsOfUse"
+          />
+        <span className="checkmark"/>
+      </label>
+      <button type="submit">Submit</button>
+    </Form>
+  )
+}
+
+const FormikUserForm = withFormik({
+  mapPropsToValues(values) {
+    return {
+      name: values.name || "",
+      email: values.email || "",
+      password: values.password || "",
+      termsOfUse: values.termsOfUse || false,
+
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+  
+  }),
+
+  handleSubmit(values) {
+
+  }
+})(UserForm);
+
+export default FormikUserForm;
